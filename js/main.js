648c4fbea0b735dcab7e3d51b6e565a08aa9a5cc
TimSchulzRC <TimSchulzRC@github.com>
2021-04-14 16:00:47 +0200

    fade in to part

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(".gallery").addClass("fade-in");
-$(".jahrgang").addClass("fade-in");
+$(".heading").addClass("fade-in");
 
 year = new Date().getFullYear();
 let copyright = `Copyright © ${year} Tim Schulz`;
 document.querySelector("footer").innerHTML += copyright;
-// Remove fade-in when not visible
+// Remove fade-in when not visibleW
