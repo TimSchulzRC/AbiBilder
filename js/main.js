bb9950da79b8b88359dc01308da49e34fb8112ba
Tim Schulz <tim.schulz.rc@gmail.com>
2021-04-14 15:03:41 +0200

    add .htaccess

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 $(".gallery").addClass("fade-in");
 $(".jahrgang").addClass("fade-in");
 
-// const options = {
-//   threshold: 0,
-//   rootMargin: "0px 0px 10px 0px",
-// };
-
-// const appearOnScroll = new IntersectionObserver((entries, observer) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       console.log("hello");
-//       entry.target.classList.add("fade-in");
-//       observer.unobserve(entry.target);
-//     } else {
-//       return;
-//     }
-//   });
-// }, options);
-
-// appearOnScroll.observe(allImages);
-
 year = new Date().getFullYear();
 let copyright = `Copyright © ${year} Tim Schulz`;
 document.querySelector("footer").innerHTML += copyright;
